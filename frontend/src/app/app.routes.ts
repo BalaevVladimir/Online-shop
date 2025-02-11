9d4b4f81053cf714b1f56fb2d637ad4272fcc733
Vladimir Balaev <vabalaev9891@gmail.com>
2025-02-11 20:49:28 +0300

    поправил

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,21 +1,21 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
+        {
         path: '',
         redirectTo: 'main',
         pathMatch: 'full',
     },
-    {
-        path: '',
-        redirectTo: 'auth',
+        {
+        path: 'main',
+        redirectTo: '/auth',
         pathMatch: 'full',
     },
-    {
+        {
         path: 'main',
         loadChildren: () => import('../app/pages/main/main.routes').then((mr) => mr.mainRoutes)
     },
-    {
+        {
         path: 'auth',
         loadChildren: () => import('../app/pages/auth/auth.routes').then((mr) => mr.authRoutes)
     },
